a0db96e656e67259e3d8ffd68fef0350052c1dd3
wangyuewen <wywlds@gmail.com>
2017-10-01 20:17:05 +0800

    modify param

--- rnn/main.py
+++ rnn/main.py
@@ -7,7 +7,6 @@ num_epoch = 50
 batcher = Batcher()
 
 def runEpoch(sess, model):
-    model.assign_new_batch_size(sess, batcher.batch_size)
     numBatch = batcher.numOfBatchPerEpoch()
     for i in range(numBatch):
         (databatch1, masks1, databatch2, masks2, labelsbatch) = batcher.next_train_batch()
@@ -21,7 +20,6 @@ def runEpoch(sess, model):
         sess.run(model.train_op, feed_dict=feed_dict)
 
 def evaluate(sess,testmodel):
-    testmodel.assign_new_batch_size(sess, 4927)
     (databatch1, masks1, databatch2, masks2, labelsbatch) = batcher.next_test_batch()
     feed_dict = {
         testmodel.input_data_s1: databatch1,
@@ -38,7 +36,7 @@ def evaluate(sess,testmodel):
     scores=batcher.test_score()
     metrics.evaluate(newScores, scores)
 
-def train(sess, model, testmodel):
+def train(sess, model):
     for i in range(num_epoch):
         runEpoch(sess, model)
         evaluate(sess, model)
@@ -47,10 +45,8 @@ def run():
     with tf.Session() as sess:
         with tf.variable_scope("model", reuse=None):
             trainmodel = Model(sess=sess)
-        with tf.variable_scope("model", reuse=True):
-            testmodel = Model(sess=sess, is_Training=False, batch_size=4927)
         tf.global_variables_initializer().run()
-        train(sess,trainmodel, testmodel)
+        train(sess,trainmodel)
 
 if __name__=="__main__":
     run()
--- rnn/model.py
+++ rnn/model.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import common
 import batcher
 class Model(object):
+    formatf = tf.float32
     # LSTM hidden state的维数
     hidden_size = 50
 
@@ -10,26 +11,26 @@ class Model(object):
     embed_dim=300
 
     # 最后做softmax的维数
-    hidden_state = 200
+    hidden_state = 50
 
     def singleCell(self, scope, cell="lstm", reuse=None):
         if cell == 'gru':
-            with tf.variable_scope("grucell" + scope, reuse=reuse, dtype=tf.float32):
+            with tf.variable_scope("grucell" + scope, reuse=reuse, dtype=self.formatf):
                 cell = tf.contrib.rnn.GRUCell(self.hidden_size, reuse = tf.get_variable_scope().reuse)
         else:
-            with tf.variable_scope("lstmcell"+scope, reuse=reuse, dtype=tf.float32):
+            with tf.variable_scope("lstmcell"+scope, reuse=reuse, dtype=self.formatf):
                 cell=tf.contrib.rnn.BasicLSTMCell(self.hidden_size, state_is_tuple=True, reuse=tf.get_variable_scope().reuse)
 
-        with tf.variable_scope("cell_init_state"+scope, reuse=reuse, dtype=tf.float32):
+        with tf.variable_scope("cell_init_state"+scope, reuse=reuse, dtype=self.formatf):
             cell_init_state=None
             #cell_init_state=cell.zero_state(self.batch_size,dtype=tf.float32)
 
         return (cell, cell_init_state)
 
     def rnn(self, x, scope, cell="lstm", reuse=None):
-        with tf.name_scope('RNN_' + scope), tf.variable_scope('RNN_' + scope, dtype=tf.float32):
+        with tf.name_scope('RNN_' + scope), tf.variable_scope('RNN_' + scope, dtype=self.formatf):
             (cell, init_state) = self.singleCell(scope, cell=cell, reuse=reuse)
-            outputs, states = tf.nn.dynamic_rnn(cell, x, initial_state=init_state, time_major=False, dtype=tf.float32)
+            outputs, states = tf.nn.dynamic_rnn(cell, x, initial_state=init_state, time_major=False, dtype=self.formatf)
         return outputs
 
     def assign_new_lr(self, session, lr_value):
@@ -42,7 +43,7 @@ class Model(object):
     def __init__(self, sess, is_Training=True, batch_size=25):
         #得到embedding
         emb = common.getEmb()
-        W = tf.Variable(emb, trainable=False, name="wdict", dtype=tf.float32)
+        W = tf.Variable(emb, trainable=False, name="wdict", dtype=self.formatf)
 
         #batch_size参数,在test时可以改变
         self.batch_size=tf.Variable(batch_size, dtype=tf.int32, trainable=False)
@@ -51,7 +52,7 @@ class Model(object):
 
         #Learning rate, 可以调整
         self.lr = tf.Variable(0.0, trainable=False)
-        self.new_lr = tf.placeholder(tf.float32, shape=[], name="new_learning_rate")
+        self.new_lr = tf.placeholder(self.formatf, shape=[], name="new_learning_rate")
         self._lr_update = tf.assign(self.lr, self.new_lr)
 
         #数据的初始化
@@ -59,9 +60,9 @@ class Model(object):
         self.input_data_s2 = tf.placeholder(tf.int32, [None, None])
         self.embedding1 = tf.nn.embedding_lookup(W, self.input_data_s1)
         self.embedding2 = tf.nn.embedding_lookup(W, self.input_data_s2)
-        self.target = tf.placeholder(tf.float32, [None, 5])
-        self.mask_s1 = tf.placeholder(tf.float32, [None, None])
-        self.mask_s2 = tf.placeholder(tf.float32, [None, None])
+        self.target = tf.placeholder(self.formatf, [None, 5])
+        self.mask_s1 = tf.placeholder(self.formatf, [None, None])
+        self.mask_s2 = tf.placeholder(self.formatf, [None, None])
 
         with tf.name_scope('lstm_output_layer'):
             if is_Training:
@@ -79,14 +80,14 @@ class Model(object):
         with tf.name_scope('loss'):
             product = tf.multiply(self.sent1, self.sent2)
             subs = tf.norm(tf.subtract(self.sent1, self.sent2), axis=1, keep_dims=True)
-            W1 = tf.get_variable(initializer=tf.random_uniform([self.hidden_state, self.hidden_size], -1.0, 1.0), name="Wpro1")
-            W2 = tf.get_variable(initializer=tf.random_uniform([self.hidden_state, 1], -1.0, 1.0), name="Wpro2")
-            self.bias = tf.get_variable(initializer=tf.constant(0.1, shape=[self.hidden_state, 1]), name="bias")
+            W1 = tf.get_variable(initializer=tf.random_uniform([self.hidden_state, self.hidden_size], -1.0, 1.0), name="Wpro1",dtype=self.formatf)
+            W2 = tf.get_variable(initializer=tf.random_uniform([self.hidden_state, 1], -1.0, 1.0), name="Wpro2",dtype=self.formatf)
+            self.bias = tf.get_variable(initializer=tf.constant(0.1, shape=[self.hidden_state, 1]), name="bias",dtype=self.formatf)
             wp = tf.matmul(W1, tf.transpose(product))+tf.matmul(W2, tf.transpose(subs))+ self.bias
             ltransform = tf.transpose(tf.sigmoid(wp))
 
-            W3 = tf.get_variable(initializer=tf.random_uniform([self.hidden_state, 5], -1.0, 1.0), name="Wpro3")
-            bias2 = tf.get_variable(initializer=tf.constant(0.1, shape=[5]), name="bias2")
+            W3 = tf.get_variable(initializer=tf.random_uniform([self.hidden_state, 5], -1.0, 1.0), name="Wpro3",dtype=self.formatf)
+            bias2 = tf.get_variable(initializer=tf.constant(0.1, shape=[5]), name="bias2",dtype=self.formatf)
             projection = tf.nn.xw_plus_b(ltransform, W3, bias2)
             psoftmax = tf.nn.softmax(projection)
             value = tf.constant([[1.0], [2.0], [3.0], [4.0], [5.0]])
